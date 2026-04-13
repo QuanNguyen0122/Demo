eab15b639a610222fa7b6c1bf7337df0cc6e573c
Nguyen Dang Minh Quan <nguyendangminhquan40@gmail.com>
2026-04-13 23:01:17 +0700

    Add file

---
+++ Object.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Car{
+public:
+    string Brand;
+    int VIN;
+    int temp;
+
+    Car(int s){
+        VIN = s;
+    }
+
+    void printInfo(){
+        cout << "Vehicle's brand: " << Brand << "." << endl;
+    }
+
+    void printVIN(){
+        cout << "Vehicle's VIN: " << VIN << "." << endl;
+    }
+};
+
+int main()
+{
+    Car car1(100);
+
+    car1.Brand = "Mercedes";
+    car1.VIN   = 12314;
+
+    car1.printInfo();
+    car1.printVIN();
+
+    return 0;
+}
+
